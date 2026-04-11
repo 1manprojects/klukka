e0c47abc9741277cb6386e70d747a4d987a5b4dc
1ManProjects <reed@1manprojects.de>
2026-04-11 14:10:14 +0200

    Fixed issue not sending reset emails

--- src/main/java/de/OneManProjects/database/Tokens.java
+++ src/main/java/de/OneManProjects/database/Tokens.java
@@ -12,10 +12,10 @@ package de.OneManProjects.database;
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -58,13 +58,13 @@ public class Tokens {
                 "INSERT INTO " + Database.TOKEN_TABLE + "(idUser,token,token_type,expiration,description) VALUES(?,?,?,?,?)",
                 userId,
                 token,
-                expiration,
                 2,
+                expiration,
                 ""
         ) > 0;
     }
 
-    public static Optional<UserToken> getToken(final String token) throws SQLException{
+    public static Optional<UserToken> getToken(final String token) throws SQLException {
         return Database.executeQuery(
                 "SELECT * FROM " + Database.TOKEN_TABLE + " WHERE token = ?",
                 Tokens::parseToken,
