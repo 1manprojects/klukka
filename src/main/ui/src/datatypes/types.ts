75d36dc66c20374a07d7ca8252c2036677c40f8c
1ManProjects <reed@1manprojects.de>
2026-05-16 15:43:20 +0200

    Updated java deps

--- src/main/ui/src/datatypes/types.ts
+++ src/main/ui/src/datatypes/types.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
-// Generated using typescript-generator version 3.2.1263 on 2026-05-05 18:53:47.
+// Generated using typescript-generator version 3.2.1263 on 2026-05-16 15:42:51.
 
 export interface Group {
     id: number;
