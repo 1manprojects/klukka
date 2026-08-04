82f86b3189b6dfb51a5991e183926f2317c5ac2e
1ManProjects <reed@1manprojects.de>
2026-08-04 18:42:24 +0200

    Added styling for export import in profile

--- src/main/ui/src/components/user/Profile.tsx
+++ src/main/ui/src/components/user/Profile.tsx
@@ -23,27 +23,42 @@
  * THE SOFTWARE.
  * #L%
  */
-import { Fragment, ReactElement, useEffect, useState } from "react"
-import { createUserToken, deleteAccount, deleteUserToken, downloadDataExport, getUserDetails, importDataExport, leaveGroup, updateEmail, updatePassword } from "../../Api";
+import {Fragment, ReactElement, useEffect, useState} from "react"
+import {
+    createUserToken,
+    deleteAccount,
+    deleteUserToken,
+    downloadDataExport,
+    getUserDetails,
+    importDataExport,
+    leaveGroup,
+    updateEmail,
+    updatePassword
+} from "../../Api";
 import './profile.scss'
-import { ExportUserData, Project, UserApiToken, UserData } from "../../datatypes/types";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDoorOpen, faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
-import { EditProject } from "../common/editProject/EditProject";
-import { getShortDate, showToast } from "../../Func";
+import {ExportUserData, Project, UserApiToken, UserData} from "../../datatypes/types";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faDoorOpen, faEdit, faTrash} from "@fortawesome/free-solid-svg-icons";
+import {EditProject} from "../common/editProject/EditProject";
+import {getShortDate, showToast} from "../../Func";
 
 
 export type DialogType = "NONE" | "PROJECT" | "GROUP";
 
 export const Profile = (): ReactElement => {
 
-    const [data, setData] = useState<UserData>({groups:[], projects:[], user: { id: -1, mail: "", roles: [] }, tokens: []});
-    const [password, setPassword] = useState<{p1: string, p2: string}>({p1: "", p2: ""});
-    const [mail, setMail] = useState<{edit: boolean, mail: string}>({edit: false, mail: ""});
+    const [data, setData] = useState<UserData>({
+        groups: [],
+        projects: [],
+        user: {id: -1, mail: "", roles: []},
+        tokens: []
+    });
+    const [password, setPassword] = useState<{ p1: string, p2: string }>({p1: "", p2: ""});
+    const [mail, setMail] = useState<{ edit: boolean, mail: string }>({edit: false, mail: ""});
     const [editProject, setProject] = useState<number>(-1);
     const [dialog, setDialog] = useState<DialogType>("NONE");
     const [newToken, setNewToken] = useState<UserApiToken>({id: -1, description: "", expiration: null});
-    const [tokenValue, setTokenValue] = useState<string| null>(null);
+    const [tokenValue, setTokenValue] = useState<string | null>(null);
     const [fileUpload, setFileUpload] = useState<File | null>(null);
 
     const fetchAndSetData = async (): Promise<void> => {
@@ -56,7 +71,7 @@ export const Profile = (): ReactElement => {
 
     useEffect(() => {
         fetchAndSetData();
-    },[])
+    }, [])
 
     const isValidEmail = (email: string): boolean => {
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -75,7 +90,7 @@ export const Profile = (): ReactElement => {
 
     const onUpdateMail = async (): Promise<void> => {
         if (isValidEmail(mail.mail)) {
-            if (window.confirm("Do you really want to change your mail to "+ mail.mail +"?\n" +
+            if (window.confirm("Do you really want to change your mail to " + mail.mail + "?\n" +
                 "You will no longer be able to log in with your old mail address")) {
                 if (window.confirm("After changing your mail you will be looged out and have to log in again with youre new mail address")) {
                     const res = await updateEmail(mail.mail);
@@ -94,7 +109,7 @@ export const Profile = (): ReactElement => {
     const onDeleteAccount = async (): Promise<void> => {
         if (window.confirm("Do you really want to delete your account?\n" +
             "This action cannot be undone and all data will be lost.")) {
-                if (window.confirm("Are you absolutely sure you want to delete your account?")) {
+            if (window.confirm("Are you absolutely sure you want to delete your account?")) {
                 const res = await deleteAccount();
                 if (res) {
                     window.sessionStorage.removeItem("authorization");
@@ -107,20 +122,20 @@ export const Profile = (): ReactElement => {
     const onLeaveGroup = async (id: number): Promise<void> => {
         if (window.confirm("Do you really want to leave this group?\n" +
             "This action cannot be undone and all data will be lost.")) {
-                const res = await leaveGroup(id);
-                if (res) {
-                    fetchAndSetData();
-                }
+            const res = await leaveGroup(id);
+            if (res) {
+                fetchAndSetData();
+            }
         }
     }
 
     const onDeleteToken = async (id: number): Promise<void> => {
         if (window.confirm("Do you really want to delete this Api-Token?\n" +
             "This action cannot be undone and you can no longer access data with this token.")) {
-                const res = await deleteUserToken(id);
-                if (res) {
-                    fetchAndSetData();
-                }
+            const res = await deleteUserToken(id);
+            if (res) {
+                fetchAndSetData();
+            }
         }
     }
 
@@ -158,12 +173,12 @@ export const Profile = (): ReactElement => {
     const render = (): ReactElement => {
         switch (dialog) {
             case "PROJECT": {
-                    const p: Project | undefined = data.projects.find((p) => p.id === editProject);
-                    if (p) {
-                        return <EditProject project={p} close={onCloseDialog}/>
-                    }
-                    return <></>
+                const p: Project | undefined = data.projects.find((p) => p.id === editProject);
+                if (p) {
+                    return <EditProject project={p} close={onCloseDialog}/>
                 }
+                return <></>
+            }
             case "GROUP":
                 return <></>
             default:
@@ -172,78 +187,89 @@ export const Profile = (): ReactElement => {
     }
 
     const renderDefault = (): ReactElement => {
-        return  <Fragment><h1>Profile</h1>
-        <div className="data">
-            <div className="profile-info">
-                <h2>User</h2>
-                <div className="detail-info">
-                    <label>E-mail:</label>
-                    {
-                        mail.edit ? 
-                        <div>
-                            <input type="text" value={mail.mail} onChange={(e) => setMail({...mail, mail: e.target.value})}/>
-                            <button onClick={onUpdateMail}>Save</button>
-                            <button onClick={() => setMail({mail:"", edit:false})}>Cancel</button>
-                        </div>
-                        : <p>{data.user.mail} <FontAwesomeIcon icon={faEdit} onClick={()=>setMail({...mail, edit: true})}/></p>
-                    }
+        return <Fragment><h1>Profile</h1>
+            <div className="data">
+                <div className="profile-info">
+                    <h2>User</h2>
+                    <div className="detail-info">
+                        <label>E-mail:</label>
+                        {
+                            mail.edit ?
+                                <div>
+                                    <input type="text" value={mail.mail}
+                                           onChange={(e) => setMail({...mail, mail: e.target.value})}/>
+                                    <button onClick={onUpdateMail}>Save</button>
+                                    <button onClick={() => setMail({mail: "", edit: false})}>Cancel</button>
+                                </div>
+                                : <p>{data.user.mail} <FontAwesomeIcon icon={faEdit}
+                                                                       onClick={() => setMail({...mail, edit: true})}/>
+                                </p>
+                        }
+                    </div>
+                    <div className="detail-info">
+                        <label>Roles:</label>
+                        <p>{data.user.roles.join(";")}</p>
+                    </div>
+                    <div className="change-password">
+                        <label>Change Password</label>
+                        <input type="password" placeholder="New Password" value={password.p1}
+                               onChange={(e) => setPassword({...password, p1: e.target.value})}/>
+                        <input type="password" placeholder="Repeat Password" value={password.p2}
+                               onChange={(e) => setPassword({...password, p2: e.target.value})}/>
+                        {password.p1 !== password.p2 ? <p className="warning">Passwords do not match</p> : null}
+                        {password.p1.length < 8 ? <p className="warning">Passwords is to short</p> : null}
+                        <button disabled={password.p1 !== password.p2} onClick={onUpdatePassword}>Change Password
+                        </button>
+                    </div>
                 </div>
-                <div className="detail-info">
-                <label>Roles:</label>
-                <p>{data.user.roles.join(";")}</p>
-                </div>
-                <div className="change-password">
-                    <label>Change Password</label>
-                    <input type="password" placeholder="New Password" value={password.p1} onChange={(e) => setPassword({...password, p1: e.target.value})}/>
-                    <input type="password" placeholder="Repeat Password" value={password.p2} onChange={(e) => setPassword({...password, p2: e.target.value})}/>
-                    {password.p1 !== password.p2 ? <p className="warning">Passwords do not match</p> : null}
-                    {password.p1.length < 8 ? <p className="warning">Passwords is to short</p> : null}
-                    <button disabled={password.p1 !== password.p2} onClick={onUpdatePassword}>Change Password</button>
-                </div>
-            </div>
-            <hr/>
-            <h2>API Tokens</h2>
+                <hr/>
+                <h2>API Tokens</h2>
                 {data.tokens.length === 0 ? <p>You do not have any Tokens</p> : null}
                 <div className="new-token">
-                    <input type="text" placeholder="Description" value={newToken.description} onChange={(e) => setNewToken({...newToken, description: e.target.value})}/>
-                    <input type="date" value={newToken.expiration? getShortDate(newToken.expiration) : ""} onChange={(e) => setNewToken({...newToken, expiration: e.target.value ? new Date(e.target.value) : null})}/>
+                    <input type="text" placeholder="Description" value={newToken.description}
+                           onChange={(e) => setNewToken({...newToken, description: e.target.value})}/>
+                    <input type="date" value={newToken.expiration ? getShortDate(newToken.expiration) : ""}
+                           onChange={(e) => setNewToken({
+                               ...newToken,
+                               expiration: e.target.value ? new Date(e.target.value) : null
+                           })}/>
                     <button disabled={newToken.description.length <= 0} onClick={onCreateApiToken}>Create Token</button>
                     {tokenValue ? <div className="token-info">
                         <span className="info-text">New Api-Token created please save it you wont be shown this token again!</span>
                         <p className="token-value">API-Token: <span>{tokenValue}</span></p>
-                        </div> : null}
+                    </div> : null}
                 </div>
                 <table className="tokens">
                     <thead>
-                        <tr>
-                            <th>Description</th>
-                            <th>Expiration</th>
-                            <th>Delete</th>
-                        </tr>
+                    <tr>
+                        <th>Description</th>
+                        <th>Expiration</th>
+                        <th>Delete</th>
+                    </tr>
                     </thead>
                     <tbody>
-                        {data.tokens.map((token, index) => (
-                            <tr key={index}>
-                                <td>{token.description}</td>
-                                <td>{token.expiration? new Date(token.expiration).toLocaleDateString() + "" : "-"}</td>
-                                <td><FontAwesomeIcon icon={faTrash} onClick={() => onDeleteToken(token.id)}/></td>
-                            </tr>
-                        ))}
+                    {data.tokens.map((token, index) => (
+                        <tr key={index}>
+                            <td>{token.description}</td>
+                            <td>{token.expiration ? new Date(token.expiration).toLocaleDateString() + "" : "-"}</td>
+                            <td><FontAwesomeIcon icon={faTrash} onClick={() => onDeleteToken(token.id)}/></td>
+                        </tr>
+                    ))}
                     </tbody>
                 </table>
-            <hr/>
-            <div className="project-info">
-                <h2>Groups</h2>
-                {data.groups.length === 0 ? <p>You are not in any group</p> : null}
-                <table>
-                    <thead>
+                <hr/>
+                <div className="project-info">
+                    <h2>Groups</h2>
+                    {data.groups.length === 0 ? <p>You are not in any group</p> : null}
+                    <table>
+                        <thead>
                         <tr>
                             <th>Title</th>
                             <th>Description</th>
                             <th>Leave Group</th>
                         </tr>
-                    </thead>
-                    <tbody>
+                        </thead>
+                        <tbody>
                         {data.groups.map((group, index) => (
                             <tr key={index}>
                                 <td>{group.title}</td>
@@ -251,27 +277,29 @@ export const Profile = (): ReactElement => {
                                 <td><FontAwesomeIcon icon={faDoorOpen} onClick={() => onLeaveGroup(group.id)}/></td>
                             </tr>
                         ))}
-                    </tbody>
-                </table>
+                        </tbody>
+                    </table>
+                </div>
+                <hr/>
+                <h2>Export / Import</h2>
+                <span>Export and Import your data (includes only personal Projects and time Tracking entries)</span>
+                <div className="export-import">
+                    <button onClick={() => downloadDataExport()}>Export Data</button>
+                    <div className="upload">
+                        <input type="file" accept=".json" onChange={(e) => setFileUpload(e.target.files?.[0] || null)}/>
+                        <button onClick={onImport}>Import Data</button>
+                    </div>
+                </div>
+                <hr/>
+                <h2>Delete Account</h2>
+                <span>This action cannot be undone</span>
+                <button className="alert" onClick={onDeleteAccount}>Delete Account</button>
             </div>
-            <hr/>
-            <h2>Export / Import</h2>
-            <span>Export and Import your data (includes only personal Projects and time Tracking entries)</span>
-            <div className="export-import">
-                <button onClick={() => downloadDataExport()}>Export Data</button>
-                <input type="file" accept=".json" onChange={(e) => setFileUpload(e.target.files?.[0] || null)} />
-                <button onClick={onImport}>Import Data</button>
-            </div>
-            <hr/>
-            <h2>Delete Account</h2>
-            <span>This action cannot be undone</span>
-            <button className="alert" onClick={onDeleteAccount}>Delete Account</button>
-        </div>
         </Fragment>
     }
 
     return <div className="profile">
-       {render()}
+        {render()}
     </div>
 
 }
